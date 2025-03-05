1e77f30b8e58c123a1009ea8642ec95a5a9c5f06
Marty <rosary-vassals8m@icloud.com>
2025-03-05 11:20:28 +0100

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -6,6 +6,9 @@ const inventory = {
         { name: 'Moscow Mule', price: 99, currency: 'CZK', image: 'images/moscow.png' },
         { name: 'Gin-Tonic', price: 99, currency: 'CZK', image: 'images/gin.png' },
         { name: 'Mojito', price: 99, currency: 'CZK', image: 'images/mojito.png' },
+        { name: 'Coca-Cola', price: 32, currency: 'CZK', image: 'images/cola.png' },
+        { name: 'Sprite', price: 32, currency: 'CZK', image: 'images/sprite.png' },
+        { name: 'Red Bull', price: 59, currency: 'CZK', image: 'images/redbull.png' },
         { name: 'Prosecco', price: 390, currency: 'CZK', image: 'images/prosecco.png' },
         { name: 'Pivo sud 30l', price: 125, currency: 'EUR', image: 'images/keg.png' },
         { name: 'Pivo sud 50l', price: 175, currency: 'EUR', image: 'images/pivo50.png' },
@@ -37,11 +40,13 @@ const inventory = {
         { name: 'Moscow Mule', price: 99, currency: 'CZK', image: 'images/moscow.png' },
         { name: 'Gin-Tonic', price: 99, currency: 'CZK', image: 'images/gin.png' },
         { name: 'Mojito', price: 99, currency: 'CZK', image: 'images/mojito.png' },
+        { name: 'Coca-Cola', price: 32, currency: 'CZK', image: 'images/cola.png' },
+        { name: 'Sprite', price: 32, currency: 'CZK', image: 'images/sprite.png' },
+        { name: 'Red Bull', price: 59, currency: 'CZK', image: 'images/redbull.png' },
         { name: 'Prosecco', price: 390, currency: 'CZK', image: 'images/prosecco.png' },
         { name: 'Pivo sud 30l', price: 125, currency: 'EUR', image: 'images/keg.png' },
         { name: 'Pivo sud 50l', price: 175, currency: 'EUR', image: 'images/pivo50.png' },
         { name: 'Plyn', price: 12, currency: 'EUR', image: 'images/Plyn.png' },
         { name: 'Gril', price: 20, currency: 'EUR', image: 'images/grill.png' },
         { name: 'Wellness', price: 0, currency: 'EUR', image: 'images/wellness.png', customPrice: true }
-    ]
-};
+    ],
