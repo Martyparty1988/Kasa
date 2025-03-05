a7d94921f7132ae6622b4239d13abc1bdac32d32
Marty <rosary-vassals8m@icloud.com>
2025-03-05 13:32:44 +0100

    Update inventory.js

--- inventory.js
+++ inventory.js
@@ -1,56 +1,35 @@
+const inventoryItems = [
+    // 1. Nealkoholické nápoje
+    { name: 'Coca-Cola', price: 32, currency: 'CZK', image: 'images/cola.png' },
+    { name: 'Sprite', price: 32, currency: 'CZK', image: 'images/sprite.png' },
+    { name: 'Fanta', price: 32, currency: 'CZK', image: 'images/fanta.png' },
+    
+    // 2. Alkoholické míchané drinky
+    { name: 'Malibu', price: 99, currency: 'CZK', image: 'images/malibu.png' },
+    { name: 'Jack s colou', price: 99, currency: 'CZK', image: 'images/jack.png' },
+    { name: 'Moscow Mule', price: 99, currency: 'CZK', image: 'images/moscow.png' },
+    { name: 'Gin-Tonic', price: 99, currency: 'CZK', image: 'images/gin.png' },
+    { name: 'Mojito', price: 99, currency: 'CZK', image: 'images/mojito.png' },
+    
+    // 3. Plechovkové nápoje
+    { name: 'Red Bull', price: 59, currency: 'CZK', image: 'images/redbull.png' },
+    { name: 'Budvar', price: 59, currency: 'CZK', image: 'images/budvar.png' },
+    
+    // 4. Prosecco
+    { name: 'Prosecco', price: 390, currency: 'CZK', image: 'images/prosecco.png' },
+    
+    // 5. Piva v sudu
+    { name: 'Pivo sud 30l', price: 125, currency: 'EUR', image: 'images/keg.png' },
+    { name: 'Pivo sud 50l', price: 175, currency: 'EUR', image: 'images/pivo50.png' },
+    
+    // 6. Wellness a grilly
+    { name: 'Plyn', price: 12, currency: 'EUR', image: 'images/Plyn.png' },
+    { name: 'Gril', price: 20, currency: 'EUR', image: 'images/grill.png' },
+    { name: 'Wellness', price: 0, currency: 'EUR', image: 'images/wellness.png', customPrice: true }
+];
+
 const inventory = {
-    'oh-yeah': [
-        { name: 'Budvar', price: 59, currency: 'CZK', image: 'images/budvar.png' },
-        { name: 'Malibu', price: 99, currency: 'CZK', image: 'images/malibu.png' },
-        { name: 'Jack s colou', price: 99, currency: 'CZK', image: 'images/jack.png' },
-        { name: 'Moscow Mule', price: 99, currency: 'CZK', image: 'images/moscow.png' },
-        { name: 'Gin-Tonic', price: 99, currency: 'CZK', image: 'images/gin.png' },
-        { name: 'Mojito', price: 99, currency: 'CZK', image: 'images/mojito.png' },
-        { name: 'Coca-Cola', price: 32, currency: 'CZK', image: 'images/cola.png' },
-        { name: 'Sprite', price: 32, currency: 'CZK', image: 'images/sprite.png' },
-        { name: 'Red Bull', price: 59, currency: 'CZK', image: 'images/redbull.png' },
-        { name: 'Prosecco', price: 390, currency: 'CZK', image: 'images/prosecco.png' },
-        { name: 'Pivo sud 30l', price: 125, currency: 'EUR', image: 'images/keg.png' },
-        { name: 'Pivo sud 50l', price: 175, currency: 'EUR', image: 'images/pivo50.png' },
-        { name: 'Plyn', price: 12, currency: 'EUR', image: 'images/Plyn.png' },
-        { name: 'Gril', price: 20, currency: 'EUR', image: 'images/grill.png' },
-        { name: 'Wellness', price: 0, currency: 'EUR', image: 'images/wellness.png', customPrice: true },
-        { name: 'Fanta', price: 32, currency: 'CZK', image: 'images/fanta.png' }
-    ],
-    'amazing-pool': [
-        { name: 'Budvar', price: 59, currency: 'CZK', image: 'images/budvar.png' },
-        { name: 'Malibu', price: 99, currency: 'CZK', image: 'images/malibu.png' },
-        { name: 'Jack s colou', price: 99, currency: 'CZK', image: 'images/jack.png' },
-        { name: 'Moscow Mule', price: 99, currency: 'CZK', image: 'images/moscow.png' },
-        { name: 'Gin-Tonic', price: 99, currency: 'CZK', image: 'images/gin.png' },
-        { name: 'Mojito', price: 99, currency: 'CZK', image: 'images/mojito.png' },
-        { name: 'Coca-Cola', price: 32, currency: 'CZK', image: 'images/cola.png' },
-        { name: 'Sprite', price: 32, currency: 'CZK', image: 'images/sprite.png' },
-        { name: 'Red Bull', price: 59, currency: 'CZK', image: 'images/redbull.png' },
-        { name: 'Prosecco', price: 390, currency: 'CZK', image: 'images/prosecco.png' },
-        { name: 'Pivo sud 30l', price: 125, currency: 'EUR', image: 'images/keg.png' },
-        { name: 'Pivo sud 50l', price: 175, currency: 'EUR', image: 'images/pivo50.png' },
-        { name: 'Plyn', price: 12, currency: 'EUR', image: 'images/Plyn.png' },
-        { name: 'Gril', price: 20, currency: 'EUR', image: 'images/grill.png' },
-        { name: 'Wellness', price: 0, currency: 'EUR', image: 'images/wellness.png', customPrice: true },
-        { name: 'Fanta', price: 32, currency: 'CZK', image: 'images/fanta.png' }
-    ],
-    'little-castle': [
-        { name: 'Budvar', price: 59, currency: 'CZK', image: 'images/budvar.png' },
-        { name: 'Malibu', price: 99, currency: 'CZK', image: 'images/malibu.png' },
-        { name: 'Jack s colou', price: 99, currency: 'CZK', image: 'images/jack.png' },
-        { name: 'Moscow Mule', price: 99, currency: 'CZK', image: 'images/moscow.png' },
-        { name: 'Gin-Tonic', price: 99, currency: 'CZK', image: 'images/gin.png' },
-        { name: 'Mojito', price: 99, currency: 'CZK', image: 'images/mojito.png' },
-        { name: 'Coca-Cola', price: 32, currency: 'CZK', image: 'images/cola.png' },
-        { name: 'Sprite', price: 32, currency: 'CZK', image: 'images/sprite.png' },
-        { name: 'Red Bull', price: 59, currency: 'CZK', image: 'images/redbull.png' },
-        { name: 'Prosecco', price: 390, currency: 'CZK', image: 'images/prosecco.png' },
-        { name: 'Pivo sud 30l', price: 125, currency: 'EUR', image: 'images/keg.png' },
-        { name: 'Pivo sud 50l', price: 175, currency: 'EUR', image: 'images/pivo50.png' },
-        { name: 'Plyn', price: 12, currency: 'EUR', image: 'images/Plyn.png' },
-        { name: 'Gril', price: 20, currency: 'EUR', image: 'images/grill.png' },
-        { name: 'Wellness', price: 0, currency: 'EUR', image: 'images/wellness.png', customPrice: true },
-        { name: 'Fanta', price: 32, currency: 'CZK', image: 'images/fanta.png' }
-    ]
+    'oh-yeah': [...inventoryItems],
+    'amazing-pool': [...inventoryItems],
+    'little-castle': [...inventoryItems]
 };
